5d0761a87c02844af22def7d40bbed628ffa6ca2
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-08-15 16:51:30 +0430

    added sequence bg color to categories index

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -16,7 +16,7 @@
     @foreach ($categories as $category)
         <a href="/categories/{{$category->id}}" class="col-lg-4 type">
             <div class="overlay-effect">
-                <h2>{{$category->category}}</h2>
+                <h2 class="font-weight-normal">{{$category->category}}</h2>
             </div>
 
             <img src="/storage/images/{{$category->category_image}}"
@@ -26,9 +26,13 @@
     @endforeach
 </div>
 
+@endsection
+
 <style>
     .header-page {
         background: url(https://images.unsplash.com/photo-1472141521881-95d0e87e2e39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80) center !important;
     }
+    .type:nth-of-type(3n-1)  .overlay-effect {
+        background: #fc4b1aa8 !important;
+    }
 </style>
-@endsection
