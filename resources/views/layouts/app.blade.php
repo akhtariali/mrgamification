24fd577ea67b51d34cce810120ec6d3af3efcc54
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-08-20 00:20:48 +0430

    debugged app.blade head tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content=@yield('description')>
+    <meta name="description" content=@yield('meta', "Mr. Gamification")>
     <meta name="keywords" content=@yield('keywords')>
     <meta name="author" content=@yield('author', 'Ali Akhtari')>
 
