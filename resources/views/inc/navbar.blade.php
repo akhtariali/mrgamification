bd0ac31eaeb037c755d20bf42ddb3a9517afae37
akhtariali <46029474+akhtariali@users.noreply.github.com>
2020-02-01 18:45:05 +0330

    removed unnecessary fonts

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,8 +6,6 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre:500" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Oleo+Script" rel="stylesheet">
 </head>
 <!-- SMALL NAVBAR -->
     <header id="header" id="home">
